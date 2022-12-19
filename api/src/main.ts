a0797302afbfc1a3ae6a5c725035c60c77b58f04
rogeliovalere <rogelio.valere.io@gmail.com>
2022-12-19 00:53:05 -0500

    added port

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,16 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // app.setGlobalPrefix('api');
-  // app.enableVersioning({
-  //   type: VersioningType.HEADER,
-  //   header: 'Api-Version',
-  //   defaultVersion: '1.0',
-  // });
-  await app.listen(3000);
+  app.enableVersioning({
+    type: VersioningType.HEADER,
+    header: 'Api-Version',
+    defaultVersion: '1.0',
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('HENRY-Connect API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
