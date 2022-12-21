bc52178b6abb015333ae2582aa35b64fd77392d3
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-12-21 18:15:10 -0300

    button styles

--- client/src/components/DropdownComponent.js
+++ client/src/components/DropdownComponent.js
@@ -1,157 +1,275 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Dropdown } from 'react-native-element-dropdown';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import axios from 'axios';
-const API_KEY = process.env.API_KEY
-const BASE_URL = process.env.BASE_URL
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import axios from "axios";
+import { TouchableOpacity } from "react-native-gesture-handler";
+const API_KEY = process.env.API_KEY;
+const BASE_URL = process.env.BASE_URL;
 // import {BASE_URL} from "@env"
 
-const data = [
-  { label: 'Item 1', value: '1' },
-  { label: 'Item 2', value: '2' },
-  { label: 'Item 3', value: '3' },
-  { label: 'Item 4', value: '4' },
-  { label: 'Item 5', value: '5' },
-  { label: 'Item 6', value: '6' },
-  { label: 'Item 7', value: '7' },
-  { label: 'Item 8', value: '8' },
-];
-
 const DropdownComponent = () => {
-    const [countryData, setCountryData]= useState([])
-    const [stateData, setStateData]= useState([])
-    const [cityData, setCityData]= useState([])
-  const [value, setValue] = useState(null);
+  const [countryData, setCountryData] = useState([]);
+  const [stateData, setStateData] = useState([]);
+  const [cityData, setCityData] = useState([]);
+  const [country, setCountry] = useState(null);
+  const [state, setState] = useState(null);
+  const [city, setCity] = useState(null);
+  const [countryName, setCountryName] = useState(null);
+  const [stateName, setStateName] = useState(null);
+  const [cityName, setCityName] = useState(null);
   const [isFocus, setIsFocus] = useState(false);
 
-  useEffect(()=> {
-var config = {
-  method: 'get',
-  url: `${BASE_URL}/countries`,
-  headers: {
-    'X-CSCAPI-KEY': 'API_KEY'
-  }
-};
-
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-  var count=Object.keys(response.data).length
-  let countryArray=[]
-  for (let i=0; i<count; i++) {
-    countryArray.push ({
-
-        value: response.data[i].iso2,
-        label: response.data[i].name,
-    })
-  }
-})
-.catch(function (error) {
-  console.log(error);
-});
-  })
-
-  const handleState =(countryCode)=> {
+  useEffect(() => {
     var config = {
-        method: 'get',
-        url: `${BASE_URL}/countries/${countryCode}/states`,
-        headers: {
-          'X-CSCAPI-KEY': 'API_KEY'
-        }
-      };
-      
-      axios(config)
+      method: "get",
+      url: `${BASE_URL}/countries`,
+      headers: {
+        "X-CSCAPI-KEY": "API_KEY",
+      },
+    };
+
+    axios(config)
       .then(function (response) {
         console.log(JSON.stringify(response.data));
+        var count = Object.keys(response.data).length;
+        let countryArray = [];
+        for (let i = 0; i < count; i++) {
+          countryArray.push({
+            value: response.data[i].iso2,
+            label: response.data[i].name,
+          });
+        }
+        setCountryData(countryArray);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }
-//   const renderLabel = () => {
-//     if (value || isFocus) {
-//       return (
-//         <Text style={[styles.label, isFocus && { color: 'blue' }]}>
-//           Dropdown label
-//         </Text>
-//       );
-//     }
-//     return null;
-//   };
+  });
+
+  const handleState = (countryCode) => {
+    var config = {
+      method: "get",
+      url: `${BASE_URL}/countries/${countryCode}/states`,
+      headers: {
+        "X-CSCAPI-KEY": "API_KEY",
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        var count = Object.keys(response.data).length;
+        let stateArray = [];
+        for (let i = 0; i < count; i++) {
+          stateArray.push({
+            value: response.data[i].iso2,
+            label: response.data[i].name,
+          });
+        }
+        setStateData(stateArray);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  //   const renderLabel = () => {
+  //     if (value || isFocus) {
+  //       return (
+  //         <Text style={[styles.label, isFocus && { color: 'blue' }]}>
+  //           Dropdown label
+  //         </Text>
+  //       );
+  //     }
+  //     return null;
+  //   };
+  const handleCity = (countryCode, stateCode) => {
+    var config = {
+      method: "get",
+      url: `${BASE_URL}/countries/${countryCode}/states/${stateCode}/cities`,
+      headers: {
+        "X-CSCAPI-KEY": "API_KEY",
+      },
+    };
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        var count = Object.keys(response.data).length;
+        let cityArray = [];
+        for (let i = 0; i < count; i++) {
+          cityArray.push({
+            value: response.data[i].id,
+            label: response.data[i].name,
+          });
+        }
+        setCityData(cityArray);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <View style={styles.container}>
       {/* {renderLabel()} */}
       {/* //opcional */}
-      {/* <StatusBar barStyle="light-content"/> */} 
-<View style={{backgroundColor:"#fff", padding: 2, borderRadius:15}}>
-
-      <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={countryData}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder={!isFocus ? 'Seleccione país' : '...'}
-        searchPlaceholder="Search..."
-        value={value}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={item => {
-            setValue(item.value);
+      {/* <StatusBar barStyle="light-content"/> */}
+      <View style={{ backgroundColor: "#fff", padding: 2, borderRadius: 15 }}>
+        <Dropdown
+          style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={countryData}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? "Seleccione país" : "..."}
+          searchPlaceholder="Search..."
+          value={country}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={(item) => {
+            setCountry(item.value);
+            handleState(item.value);
+            setCountryName(item.label)
             setIsFocus(false);
-        }}
-            />
-                  <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={data}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder={!isFocus ? 'Seleccione provincia o estado' : '...'}
-        searchPlaceholder="Search..."
-        value={value}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={item => {
-            setValue(item.value);
+          }}
+        />
+        <Dropdown
+          style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={stateData}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? "Seleccione provincia o estado" : "..."}
+          searchPlaceholder="Search..."
+          value={state}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={(item) => {
+            setState(item.value);
+            handleCity(country, item.value);
+            setStateName(item.label)
             setIsFocus(false);
-        }}
-            />
-                  <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={data}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder={!isFocus ? 'Seleccione ciudad' : '...'}
-        searchPlaceholder="Search..."
-        value={value}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={item => {
-            setValue(item.value);
+          }}
+        />
+        <Dropdown
+          style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={cityData}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? "Seleccione ciudad" : "..."}
+          searchPlaceholder="Search..."
+          value={city}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={(item) => {
+            setCity(item.value);
+            setCityName(item.label)
             setIsFocus(false);
-        }}
-            />
+          }}
+        />
+    {/* <View
+          style={{
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+            paddingVertical: 5,
+          }}>
+          <TouchableOpacity
+            onPress={() =>
+              navigation.push('EditProfile', {
+                name: name,
+                profileImage: profileImage,
+              })
+            }
+            style={{
+              width: '100%',
+            }}>
+            <View
+              style={{
+                width: '100%',
+                height: 35,
+                borderRadius: 5,
+                borderColor: '#DEDEDE',
+                borderWidth: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: 14,
+                  letterSpacing: 1,
+                  opacity: 0.8,
+                }}>
+                Edit Profile
+              </Text>
             </View>
+          </TouchableOpacity>
+        </View> */}
+<View
+        style={{
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+            paddingVertical: 5,}}
+            >
+
+        <TouchableOpacity
+          style={{
+              backgroundColor: "#833CF0",
+              padding: 20,
+              borderRadius: 20,
+              alignItems: "center",
+            }}
+            onPress={() => Alert.alert(`Elegiste ${cityName}, ${stateName}, ${countryName}`)}
+            // navigation.navigate("HomeScreen")}
+            >
+                <View
+                style={{
+                width: '100%',
+                height: 30,
+                // padding: 30,
+
+                borderRadius: 5,
+                
+                // borderColor: '#DEDEDE',
+                // borderWidth: 1,
+                justifyContent: 'center',
+                alignItems: 'center'}}>
+          <Text
+            style={{
+                color: "#ffffff",
+                // paddingVertical: 20,
+                textTransform: "uppercase",
+                //   fontWeight: "00",
+                fontSize: 16
+            }}
+            >
+            ACEPTAR
+          </Text>
+              </View>
+        </TouchableOpacity>
+              </View>
+      </View>
     </View>
   );
 };
@@ -161,25 +279,25 @@ export default DropdownComponent;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#AF84F1',
+    backgroundColor: "#AF84F1",
     padding: 16,
     justifyContent: "center",
-    alignContent: "center"
+    alignContent: "center",
   },
   dropdown: {
     height: 50,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 0.5,
     borderRadius: 8,
     paddingHorizontal: 8,
-    marginBottom:10
+    marginBottom: 10,
   },
   icon: {
     marginRight: 5,
   },
   label: {
-    position: 'absolute',
-    backgroundColor: 'white',
+    position: "absolute",
+    backgroundColor: "white",
     left: 22,
     top: 8,
     zIndex: 999,
