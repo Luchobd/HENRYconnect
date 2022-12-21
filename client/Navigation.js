5b82b4bb1819870f0c6f7ffde59b1c33290afdd4
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-12-20 22:01:11 -0300

    solving folder

--- client/Navigation.js
+++ client/Navigation.js
@@ -1,59 +1,64 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { Ionicons } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 //screens
-import HomeScreen from "./screens/HomeScreen";
-import LocationScreen from "./screens/LocationScreen";
-import StackScreen from "./screens/StackScreen";
-import UserScreen from "./screens/UserScreen";
-
+import HomeScreen from "./src/screens/HomeScreen";
+import LocationScreen from "./src/screens/LocationScreen";
+import StackScreen from "./src/screens/StackScreen";
+import UserScreen from "./src/screens/UserScreen";
 
 const Tab = createBottomTabNavigator();
 
-function MyTabs(){
-    return(
-        <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-            tabBarStyle: {
-                backgroundColor:"#5947C2"
-            },
-            tabBarActiveBackgroundColor: "#5037B6",
-            tabBarActiveTintColor: "white"
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "#5947C2",
+        },
+        tabBarActiveBackgroundColor: "#5037B6",
+        tabBarActiveTintColor: "white",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
         }}
-        >
-            <Tab.Screen name="Home" component={HomeScreen}
-             options={{
-                tabBarIcon: ({color, size}) =>(
-                    <Ionicons name="home-outline" size={size} color={color} />
-                )
-            }}
-            />
-            <Tab.Screen name="Location" component={LocationScreen}
-            options={{
-                tabBarIcon: ({color, size}) =>(
-                    <Ionicons name="ios-compass-outline" size={size} color={color} />
-                )
-            }}
-            />
-            <Tab.Screen name="User" component={UserScreen}
-            options={{
-                tabBarIcon: ({color, size}) =>(
-                    <Feather name="user" size={size} color={color} />
-                )
-            }}
-            />
-        </Tab.Navigator>
-    )
+      />
+      <Tab.Screen
+        name="Location"
+        component={LocationScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-compass-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 export default function Navigation() {
-    return(
-        <NavigationContainer>
-            <MyTabs/>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
 }
