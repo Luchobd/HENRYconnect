d680e7c244556d5860f65b9aa2002ca00176baf6
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-12-21 15:01:56 -0300

    dropDown menu for selecting location

--- client/src/screens/LocationScreen.js
+++ client/src/screens/LocationScreen.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import DropdownComponent from "../components/DropdownComponent";
 
 const LocationScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <View>
+
       <Text style={{
         fontSize: 30,
         textAlign: "center",
         marginTop: "20%"
       }}
-      >Location Screen</Text>
+      >Seleccione su ubicación</Text>
+      <DropdownComponent/>
+      </View>
     </View>
   );
 }
 
 export default LocationScreen;
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#AF84F1',
+
+  },
+})
